db213b8fb1047a50a2f0b0bb02a8a800afbecfe1
Drew <drewclementsdesign@gmail.com>
2022-09-30 14:31:34 -0400

    more clean up

--- resources/js/Pages/Auth/CreateAccount.vue
+++ resources/js/Pages/Auth/CreateAccount.vue
@@ -1,9 +1,14 @@
+<script>
+import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+export default {
+    layout: DefaultLayout,
+};
+</script>
 <script setup>
 // utils
 import { Head, useForm } from "@inertiajs/inertia-vue3";
 
 //components
-import DefaultLayout from "@/Layouts/DefaultLayout.vue";
 import BaseHeading from "@/Components/base/BaseHeading.vue";
 import BaseInput from "@/Components/base/BaseInput.vue";
 import BaseButton from "@/Components/base/BaseButton.vue";
@@ -34,139 +39,118 @@ const submit = () => {
 <template>
     <Head title="Create an account" />
 
-    <DefaultLayout>
-        <section>
-            <div
-                class="
-                    max-w-4xl
-                    mx-auto
-                    px-6
-                    xl:px-0
-                    grid grid-cols-1
-                    lg:grid-cols-2
-                    gap-24
-                    items-center
-                "
-            >
-                <form class="mt-12" @submit.prevent="submit">
-                    <BaseHeading class="mb-5" size="h4" tag="h1">
-                        Create an account
-                    </BaseHeading>
+    <section>
+        <div
+            class="max-w-4xl mx-auto px-6 xl:px-0 grid grid-cols-1 lg:grid-cols-2 gap-24 items-center"
+        >
+            <form class="mt-12" @submit.prevent="submit">
+                <BaseHeading class="mb-5" size="h4" tag="h1">
+                    Create an account
+                </BaseHeading>
 
-                    <div class="flex flex-col space-y-6">
-                        <div class="relative">
-                            <BaseInput v-model="form.username" required>
-                                Username
-                            </BaseInput>
+                <div class="flex flex-col space-y-6">
+                    <div class="relative">
+                        <BaseInput v-model="form.username" required>
+                            Username
+                        </BaseInput>
 
-                            <InputError
-                                class="mt-2"
-                                :message="form.errors.username"
-                            />
-                        </div>
-
-                        <div class="relative">
-                            <BaseInput v-model="form.name" required>
-                                Name
-                            </BaseInput>
-
-                            <InputError
-                                class="mt-2"
-                                :message="form.errors.name"
-                            />
-                        </div>
-
-                        <div class="relative">
-                            <BaseInput v-model="form.email" required>
-                                Email
-                            </BaseInput>
-
-                            <InputError
-                                class="mt-2"
-                                :message="form.errors.email"
-                            />
-                        </div>
-
-                        <div class="relative">
-                            <BaseInput
-                                v-model="form.password"
-                                inputType="password"
-                                required
-                            >
-                                Password
-                            </BaseInput>
-
-                            <InputError
-                                class="mt-2"
-                                :message="form.errors.password"
-                            />
-                        </div>
-
-                        <div class="relative">
-                            <BaseInput
-                                v-model="form.password_confirmation"
-                                inputType="password"
-                                required
-                            >
-                                Confirm Password
-                            </BaseInput>
-
-                            <InputError
-                                class="mt-2"
-                                :message="form.errors.password_confirmation"
-                            />
-                        </div>
-
-                        <div class="relative">
-                            <input
-                                id="terms"
-                                name="terms"
-                                v-model="form.terms"
-                                type="checkbox"
-                                class="mr-1"
-                            />
-                            <label for="terms">
-                                I agree to the
-                                <router-link
-                                    to="/terms-and-conditions"
-                                    class="text-blue-500 underline"
-                                >
-                                    terms and conditions
-                                </router-link>
-                                of Heygumball
-                            </label>
-                            <InputError
-                                class="mt-2"
-                                :message="form.errors.terms"
-                            />
-                        </div>
-
-                        <div class="w-full lg:w-auto ml-auto">
-                            <BaseButton
-                                type="submit"
-                                theme="tertiary"
-                                class="w-full"
-                                :disabled="
-                                    form.processing === 'submitting' ||
-                                    !form.terms
-                                "
-                            >
-                                {{
-                                    form.processing === "submitting"
-                                        ? "Submitting"
-                                        : "Create Account"
-                                }}
-                            </BaseButton>
-                        </div>
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.username"
+                        />
                     </div>
-                </form>
 
-                <div class="relative">
-                    <CreateAccount class="w-full" />
+                    <div class="relative">
+                        <BaseInput v-model="form.name" required>
+                            Name
+                        </BaseInput>
+
+                        <InputError class="mt-2" :message="form.errors.name" />
+                    </div>
+
+                    <div class="relative">
+                        <BaseInput v-model="form.email" required>
+                            Email
+                        </BaseInput>
+
+                        <InputError class="mt-2" :message="form.errors.email" />
+                    </div>
+
+                    <div class="relative">
+                        <BaseInput
+                            v-model="form.password"
+                            inputType="password"
+                            required
+                        >
+                            Password
+                        </BaseInput>
+
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.password"
+                        />
+                    </div>
+
+                    <div class="relative">
+                        <BaseInput
+                            v-model="form.password_confirmation"
+                            inputType="password"
+                            required
+                        >
+                            Confirm Password
+                        </BaseInput>
+
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.password_confirmation"
+                        />
+                    </div>
+
+                    <div class="relative">
+                        <input
+                            id="terms"
+                            name="terms"
+                            v-model="form.terms"
+                            type="checkbox"
+                            class="mr-1"
+                        />
+                        <label for="terms">
+                            I agree to the
+                            <router-link
+                                to="/terms-and-conditions"
+                                class="text-blue-500 underline"
+                            >
+                                terms and conditions
+                            </router-link>
+                            of Heygumball
+                        </label>
+                        <InputError class="mt-2" :message="form.errors.terms" />
+                    </div>
+
+                    <div class="w-full lg:w-auto ml-auto">
+                        <BaseButton
+                            type="submit"
+                            theme="tertiary"
+                            class="w-full"
+                            :disabled="
+                                form.processing === 'submitting' || !form.terms
+                            "
+                        >
+                            {{
+                                form.processing === "submitting"
+                                    ? "Submitting"
+                                    : "Create Account"
+                            }}
+                        </BaseButton>
+                    </div>
                 </div>
+            </form>
+
+            <div class="relative">
+                <CreateAccount class="w-full" />
             </div>
-        </section>
-    </DefaultLayout>
+        </div>
+    </section>
 </template>
 
 <style scoped>
--- resources/js/Pages/Auth/SignIn.vue
+++ resources/js/Pages/Auth/SignIn.vue
@@ -1,9 +1,15 @@
+<script>
+import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+export default {
+    layout: DefaultLayout,
+};
+</script>
+
 <script setup>
 // utils
 import { Head, useForm } from "@inertiajs/inertia-vue3";
 
 //components
-import DefaultLayout from "@/Layouts/DefaultLayout.vue";
 import BaseHeading from "@/Components/base/BaseHeading.vue";
 import BaseInput from "@/Components/base/BaseInput.vue";
 import InputError from "@/Components/InputError.vue";
@@ -30,69 +36,55 @@ const submit = () => {
 
 <template>
     <Head title="Sign in" />
-    <DefaultLayout>
-        <section>
-            <div
-                class="
-                    max-w-4xl
-                    mx-auto
-                    px-6
-                    xl:px-0
-                    grid grid-cols-1
-                    lg:grid-cols-2
-                    gap-24
-                    items-center
-                "
-            >
-                <form class="mt-12" @submit.prevent="submit">
-                    <BaseHeading class="mb-5" size="h4" tag="h1"
-                        >Welcome back!</BaseHeading
-                    >
+    <section>
+        <div
+            class="max-w-4xl mx-auto px-6 xl:px-0 grid grid-cols-1 lg:grid-cols-2 gap-24 items-center"
+        >
+            <form class="mt-12" @submit.prevent="submit">
+                <BaseHeading class="mb-5" size="h4" tag="h1"
+                    >Welcome back!</BaseHeading
+                >
 
-                    <div class="flex flex-col space-y-6">
-                        <div class="relative">
-                            <BaseInput v-model="form.email" required>
-                                Email
-                            </BaseInput>
+                <div class="flex flex-col space-y-6">
+                    <div class="relative">
+                        <BaseInput v-model="form.email" required>
+                            Email
+                        </BaseInput>
 
-                            <InputError
-                                class="mt-2"
-                                :message="form.errors.email"
-                            />
-                        </div>
-
-                        <div class="relative">
-                            <BaseInput
-                                inputType="password"
-                                required
-                                v-model="form.password"
-                            >
-                                Password
-                            </BaseInput>
-
-                            <InputError
-                                class="mt-2"
-                                :message="form.errors.password"
-                            />
-                        </div>
-
-                        <div class="w-full">
-                            <BaseButton
-                                class="w-full lg:w-auto ml-auto"
-                                type="submit"
-                                theme="tertiary"
-                                :disabled="form.processing"
-                            >
-                                Sign In
-                            </BaseButton>
-                        </div>
+                        <InputError class="mt-2" :message="form.errors.email" />
                     </div>
-                </form>
 
-                <div class="relative">
-                    <WelcomeBack class="w-full" />
+                    <div class="relative">
+                        <BaseInput
+                            inputType="password"
+                            required
+                            v-model="form.password"
+                        >
+                            Password
+                        </BaseInput>
+
+                        <InputError
+                            class="mt-2"
+                            :message="form.errors.password"
+                        />
+                    </div>
+
+                    <div class="w-full">
+                        <BaseButton
+                            class="w-full lg:w-auto ml-auto"
+                            type="submit"
+                            theme="tertiary"
+                            :disabled="form.processing"
+                        >
+                            Sign In
+                        </BaseButton>
+                    </div>
                 </div>
+            </form>
+
+            <div class="relative">
+                <WelcomeBack class="w-full" />
             </div>
-        </section>
-    </DefaultLayout>
+        </div>
+    </section>
 </template>
