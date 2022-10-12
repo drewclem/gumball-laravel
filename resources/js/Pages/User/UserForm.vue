9da34644ee067c7091018831495d8faf2a77ce4d
Drew <drewclementsdesign@gmail.com>
2022-10-12 00:08:16 -0400

    A whole lot

---
+++ resources/js/Pages/User/UserForm.vue
@@ -0,0 +1,306 @@
+<script>
+import DefaultLayout from "@/Layouts/DefaultLayout.vue";
+export default {
+    layout: DefaultLayout,
+};
+</script>
+
+<script setup>
+// utils
+import { computed } from "vue";
+import useFileList from "@/utils/file-list";
+import { Head, useForm } from "@inertiajs/inertia-vue3";
+
+// recaptcha
+import { VueRecaptcha } from "vue-recaptcha";
+
+// components
+import BaseHeading from "@/components/base/BaseHeading.vue";
+import BaseText from "@/components/base/BaseText.vue";
+import BaseInput from "@/components/base/BaseInput.vue";
+import BaseRichText from "@/components/base/BaseRichText.vue";
+import BaseButton from "@/components/base/BaseButton.vue";
+import BaseImage from "@/components/base/BaseImage.vue";
+import BaseDropzone from "@/components/base/BaseDropzone.vue";
+import BaseFilePreview from "@/components/base/BaseFilePreview.vue";
+
+const props = defineProps({
+    collection: Object,
+    user: Object,
+});
+
+const { files, addFiles, removeFile } = useFileList();
+
+const form = useForm({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+    images: [],
+    toc: false,
+    recaptcha: false,
+});
+
+const sitekey = import.meta.env.VITE_RECAPTCHA_SITE_KEY
+
+const hasCollection = computed(() => {
+    return props.collection.length > 0;
+});
+</script>
+
+<template>
+    <div class="relative">
+        <div class="absolute bg-gray-100 top-0 w-full h-56" />
+
+        <transition name="fade" appear>
+            <div class="relative max-w-lg mx-auto mb-12">
+                <div
+                    class="flex justify-between items-center py-8 lg:py-12 px-6 lg:p-11"
+                >
+                    <BaseHeading size="h4" tag="h1">
+                        Contact
+                        <span class="text-blue-500">
+                            <span>@{{ user.username }}</span>
+                        </span>
+                    </BaseHeading>
+
+                    <div
+                        class="h-16 w-16 bg-gray-400 rounded-full overflow-hidden"
+                    >
+                        <!-- <BaseImage
+                            v-if="state.avatar_url !== null"
+                            :src="state.avatar_url"
+                            :alt="username"
+                            class="h-16 w-16 object-cover"
+                        /> -->
+                    </div>
+                </div>
+
+                <div
+                    class="card-shadow bg-white rounded-xl mx-6 lg:mx-0 p-6 lg:p-11"
+                >
+                    <div
+                        class="flex flex-col space-y-6"
+                        v-if="hasCollection && user.prescreen !== null"
+                    >
+                        <BaseHeading size="h3" tag="h2">
+                            Read first:
+                        </BaseHeading>
+
+                        <div v-html="currentUser.prescreen" />
+
+                        <div>
+                            <BaseButton
+                                @click="showForm = true"
+                                theme="tertiary"
+                            >
+                                Confirm
+                            </BaseButton>
+                        </div>
+                    </div>
+
+                    <div v-else>
+                        <form
+                            class="flex flex-col gap-8"
+                            @submit.prevent="submitForm"
+                        >
+                            <div class="relative">
+                                <BaseInput v-model="form.name" required>
+                                    Name
+                                </BaseInput>
+
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.name"
+                                />
+                            </div>
+
+                            <div class="relative">
+                                <BaseInput
+                                    v-model="form.email"
+                                    input-type="email"
+                                    required
+                                >
+                                    Email
+                                </BaseInput>
+
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.email"
+                                />
+                            </div>
+
+                            <div class="relative">
+                                <BaseInput
+                                    v-model="form.phone"
+                                    input-type="tel"
+                                    required
+                                >
+                                    Phone
+                                </BaseInput>
+
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.phone"
+                                />
+                            </div>
+
+                            <div class="relative">
+                                <BaseRichText v-model="form.message" required>
+                                    Message
+                                </BaseRichText>
+
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.message"
+                                />
+                            </div>
+
+                            <div>
+                                <p class="font-display text-sm">Add images</p>
+                                <p class="text-xs mb-4 opacity-50">
+                                    Accepted file types: jpeg, jpg, png
+                                </p>
+                                <BaseDropzone
+                                    @files-dropped="addFiles"
+                                    #default="{ dropZoneActive }"
+                                >
+                                    <label class="text-sm" for="file-input">
+                                        <span v-if="dropZoneActive">
+                                            <span>Drop Them Here</span>
+                                        </span>
+                                        <span v-else>
+                                            <span>Drag Your Files Here</span>
+                                            <span class="text-sm">
+                                                or
+                                                <strong class="text-red-500"
+                                                    ><em>click here</em></strong
+                                                >
+                                                to select files
+                                            </span>
+                                        </span>
+
+                                        <input
+                                            type="file"
+                                            id="file-input"
+                                            accept="image/*"
+                                            multiple
+                                            @change="onInputChange"
+                                        />
+                                    </label>
+
+                                    <ul
+                                        class="grid grid-cols-4 gap-2 mt-3"
+                                        v-show="files.length"
+                                    >
+                                        <BaseFilePreview
+                                            v-for="file of files"
+                                            :key="file.id"
+                                            :file="file"
+                                            tag="li"
+                                            @remove="removeFile"
+                                        />
+                                    </ul>
+                                </BaseDropzone>
+                            </div>
+
+                            <div class="border-t-2 border-gray-100" />
+
+                            <div>
+                                <VueRecaptcha
+                                    :sitekey="sitekey"
+                                    :load-recaptcha-script="true"
+                                    @verify="form.recaptcha = true"
+                                />
+                            </div>
+
+                            <div class="relative">
+                                <input
+                                    id="terms"
+                                    name="terms"
+                                    v-model="form.terms"
+                                    type="checkbox"
+                                    class="mr-1"
+                                />
+                                <label for="terms">
+                                    I agree to the
+                                    <router-link
+                                        to="/terms-and-conditions"
+                                        class="text-blue-500 underline"
+                                    >
+                                        terms and conditions
+                                    </router-link>
+                                    of Heygumball
+                                </label>
+                                <InputError
+                                    class="mt-2"
+                                    :message="form.errors.terms"
+                                />
+                            </div>
+
+                            <div class="lg:ml-auto">
+                                <BaseButton
+                                    class="w-full"
+                                    theme="tertiary"
+                                    type="submit"
+                                    :disabled="
+                                        form.processing === 'submitting' ||
+                                        !form.toc ||
+                                        !form.recaptcha
+                                    "
+                                >
+                                    Submit
+                                </BaseButton>
+                            </div>
+                        </form>
+
+                        <!-- <div v-else-if="formState === 'submitted'">
+                            <BaseHeading
+                                tag="h2"
+                                size="h3"
+                                class="text-green-500 mb-5"
+                            >
+                                Thanks for submitting!
+                            </BaseHeading>
+
+                            <BaseText>
+                                <span>
+                                    {{ `@${username}` }}
+                                </span>
+                                will be in touch!</BaseText
+                            >
+                        </div> -->
+
+                        <!-- <div v-else>
+                            <BaseHeading
+                                tag="h2"
+                                size="h3"
+                                class="text-red-500 mb-5"
+                            >
+                                Uh oh!
+                            </BaseHeading>
+
+                            <BaseText>
+                                It looks like their books are currently closed.
+                            </BaseText>
+                            <BaseText>Please try again later.</BaseText>
+                        </div> -->
+                    </div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+    opacity: 1;
+    transition: 300ms ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
