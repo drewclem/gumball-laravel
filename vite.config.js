dced5cc3ebbd7d311bdabc2b1b8ac7660db70598
Drew Clements <drewclementsdesign@gmail.com>
2023-01-09 15:27:24 -0500

    revert

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,33 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
-export default ({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-    return defineConfig({
-        server: {
-            host: "localhost",
-        },
-        plugins: [
-            laravel({
-                input: "resources/js/app.js",
-                ssr: "resources/js/ssr.js",
-                refresh: true,
-            }),
-            vue({
-                template: {
-                    transformAssetUrls: {
-                        base: null,
-                        includeAbsolute: false,
-                    },
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: "resources/js/app.js",
+            ssr: "resources/js/ssr.js",
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
                 },
-            }),
-        ],
-        ssr: {
-            noExternal: ["@inertiajs/server"],
-        },
-        build: {
-            rollupOptions: {
-                external: [
-                    "vue-recaptcha",
-                    "@vueup/vue-quill",
-                    "@vueup/vue-quill/dist/vue-quill.snow.css",
-                ],
             },
+        }),
+    ],
+    ssr: {
+        noExternal: ["@inertiajs/server"],
+    },
+    build: {
+        rollupOptions: {
+            external: [
+                "vue-recaptcha",
+                "@vueup/vue-quill",
+                "@vueup/vue-quill/dist/vue-quill.snow.css",
+            ],
         },
-    });
-};
+    },
+});
