75d5d2ea551001ddcb29494b84bf8d1e62bd891b
Drew Clements <drewclementsdesign@gmail.com>
2023-01-09 15:22:33 -0500

    trying something

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,40 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: "resources/js/app.js",
-            ssr: "resources/js/ssr.js",
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
-    ssr: {
-        noExternal: ["@inertiajs/server"],
-    },
-    build: {
-        rollupOptions: {
-            external: [
-                "vue-recaptcha",
-                "@vueup/vue-quill",
-                "@vueup/vue-quill/dist/vue-quill.snow.css",
-            ],
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return defineConfig({
+        server: {
+            host: process.env.VITE_APP_SERVER,
         },
-    },
-});
+        plugins: [
+            laravel({
+                input: "resources/js/app.js",
+                ssr: "resources/js/ssr.js",
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+        ssr: {
+            noExternal: ["@inertiajs/server"],
+        },
+        build: {
+            rollupOptions: {
+                external: [
+                    "vue-recaptcha",
+                    "@vueup/vue-quill",
+                    "@vueup/vue-quill/dist/vue-quill.snow.css",
+                ],
+            },
+        },
+    });
+};
